92055c244d7c9bf496e8367e511e28a555a5a088
sitozeke <99953640+sitozeke@users.noreply.github.com>
2022-02-18 22:24:48 +0100

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { createStore } from 'redux';
+import allReducer from'./reducer';
+
+const store = createStore(allReducer,
+    +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render (<App />, document.getElementById('root'));
